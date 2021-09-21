e66e94ba278db75a9dbbfca856eee5e5ede6da9d
Mickie <mken.mickie@gmail.com>
2021-09-21 23:05:00 +0900

    最初のテストの作成

---
+++ test/swing_calc_for_education/mickie/education/calc/cpu/FormulaTest.java
@@ -0,0 +1,31 @@
+package swing_calc_for_education.mickie.education.calc.cpu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import personal.mickie.education.calc.cpu.Formula;
+import personal.mickie.education.calc.cpu.Key;
+
+public class FormulaTest {
+
+	private Formula formula;
+
+	@Before
+	public void InitializeFormula() {
+		formula = new Formula();
+	}
+
+	@Test
+	public void SimpleFormulatest() {
+		// 何も入れなければ0を返す
+		assertEquals(formula.GetResult(), 0);
+		
+		// 1のキーを式に送信したら1を返す
+		formula = formula.AddKey(Key.GetFromString("1"));
+		assertEwuals(formula.GetResult(), 1);
+		
+	}
+
+}
